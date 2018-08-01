3bbc7006141097744e486943528de74e49906255
Hasala Marakkalage <hasalams@gmail.com>
2018-08-01 11:52:55 +0800

    Add code folder
    Added code folder and Arduino codes for two GPS receiver devices

---
+++ Code/Device1/Device1.ino
@@ -0,0 +1,209 @@
+
+//Ethernet Libraries
+#include <SPI.h>         // needed for Arduino versions later than 0018
+#include <Ethernet2.h>
+#include <EthernetUdp2.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
+//GPS Libraries
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(8, 7);
+
+Adafruit_GPS GPS(&mySerial);
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences. 
+#define GPSECHO  true
+
+// this keeps track of whether we're using the interrupt
+// off by default!
+boolean usingInterrupt = false;
+void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
+//
+String gpsID = "GPS001";
+String data;
+//Ethernet Variables
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0x90, 0xA2, 0xDA, 0x0F, 0xF8, 0xA7 // device 1 mac
+};
+EthernetClient client;
+
+unsigned int localPort = 8888;      // local port to listen on
+
+// buffers for receiving and sending data
+///char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
+///char  ReplyBuffer[] = "acknowledged";       // a string to send back
+
+// An EthernetUDP instance to let us send and receive packets over UDP
+///EthernetUDP Udp;
+//GPS variables
+void setup() {
+  // put your setup code here, to run once:
+  //========Ethernet Setup======
+  Ethernet.begin(mac);
+//  Udp.begin(localPort);
+  //Serial.begin(9600); //not using at the moment
+  //========GPS Setup========
+   // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  // also spit it out
+  //Serial.begin(115200);
+  Serial.begin(57600);
+  Serial.println("Begin location over Ethenet");
+
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+  
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+  
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we don't suggest using anything higher than 1 Hz
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  // the nice thing about this code is you can have a timer0 interrupt go off
+  // every 1 millisecond, and read data from the GPS for you. that makes the
+  // loop code a heck of a lot easier!
+  useInterrupt(true);
+
+  delay(1000);
+  // Ask for firmware version
+  mySerial.println(PMTK_Q_RELEASE);
+  //
+  // print your local IP address:
+  Serial.print("My IP address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print(".");
+  }
+  Serial.println();
+  //
+  data = "";
+}
+
+//before loop
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+#ifdef UDR0
+  if (GPSECHO)
+    if (c) UDR0 = c;  
+    // writing direct to UDR0 is much much faster than Serial.print 
+    // but only one character can be written at a time. 
+#endif
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+
+uint32_t timer = millis();
+//end before loop
+
+void loop() {
+ //begin GPS code
+   // in case you are not using the interrupt above, you'll
+  // need to 'hand query' the GPS, not suggested :(
+  if (! usingInterrupt) {
+    // read data from the GPS in the 'main loop'
+    char c = GPS.read();
+    // if you want to debug, this is a good time to do it!
+    if (GPSECHO)
+      if (c) Serial.print(c);
+  }
+  
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences! 
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+  
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis())  timer = millis();
+
+  // approximately every 60 seconds or so, print out the current stats
+  if (millis() - timer > 60000) { 
+    timer = millis(); // reset the timer
+    
+    //Serial.print("\nTime: ");
+    //Serial.print(GPS.hour, DEC); Serial.print(':');
+    //Serial.print(GPS.minute, DEC); Serial.print(':');
+    //Serial.print(GPS.seconds, DEC); Serial.print('.');
+    //Serial.println(GPS.milliseconds);
+    //Serial.print("Date: ");
+    //Serial.print(GPS.day, DEC); Serial.print('/');
+    //Serial.print(GPS.month, DEC); Serial.print("/20");
+    //Serial.println(GPS.year, DEC);
+    //Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+    ///fixValue = (int)GPS.fixquality;
+    if (GPS.fix) {
+      //Serial.print("Location: ");
+      //Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      //Serial.print(", "); 
+      //Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.print("Location (in degrees, works with Google Maps): ");
+      Serial.print(GPS.latitudeDegrees, 4);
+      Serial.print(", "); 
+      Serial.println(GPS.longitudeDegrees, 4);
+      //Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      //Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      //Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+      //
+      String latVal = String(GPS.latitudeDegrees, 8);
+      String lonVal = String(GPS.longitudeDegrees, 8);
+      float altitudeVal = GPS.altitude;
+      data = gpsID + "," + lonVal + "," + latVal + "," + "0" + "," + altitudeVal;
+    
+
+      //
+         Serial.print("sending data : "+data);
+      //begin communication
+	if (client.connect("202.94.70.33",80)) { // REPLACE WITH YOUR SERVER ADDRESS
+		client.println("GET /server_testing/insertAPData.php?data="+data); 
+		client.println("Host: 202.94.70.33"); // SERVER ADDRESS HERE TOO
+		client.println("Content-Type: application/x-www-form-urlencoded"); 
+		client.print("Content-Length: "); 
+		client.println(data.length()); 
+		client.println(); 
+		client.print(data); 
+	} 
+
+	if (client.connected()) { 
+		client.stop();	// DISCONNECT FROM THE SERVER
+	}
+
+	///delay(2000); // WAIT FIVE MINUTES BEFORE SENDING AGAIN
+      //end communication
+      //
+      //
+    }
+  }
+  //end GPS code 
+
+}
---
+++ Code/Readme.txt
@@ -0,0 +1,4 @@
+The differeces between Device1 code and Device2 code are as follows.
+
+GPS IDs are different.
+MAC Addresses are different.
